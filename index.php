2a26b43f9814afd2fc428cf5ccd9d7b6fa26935a
Lipsum <m.margaritapopova@gmail.com>
2022-04-05 09:44:31 +0300

    Вынеси хэдер в отдельный шаблон

--- index.php
+++ index.php
@@ -4,13 +4,20 @@ require_once 'data.php';
 require_once 'functions.php';
 
 $page_title = 'readme: популярное';
-$page_content = include_template('main.php', ['popular_posts' => $popular_posts]);
+
+$page_header = include_template('header.php', [
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+]);
+
+$page_content = include_template('main.php', [
+    'popular_posts' => $popular_posts
+]);
+
 $layout_content = include_template('layout.php', [
-        'page_title' => $page_title,
-        'is_auth' => $is_auth,
-        'user_name' => $user_name,
-        'page_content' => $page_content,
-    ]
-);
+    'page_title' => $page_title,
+    'page_header' => $page_header,
+    'page_content' => $page_content,
+]);
 
 print($layout_content);
